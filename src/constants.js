a6248c33f0bca38fa1de166000571640ee121d78
Rene Dellefont <renedellefont@gmail.com>
2020-05-01 16:51:53 -0400

    Add fall rate constants.

--- src/constants.js
+++ src/constants.js
@@ -13,8 +13,15 @@ export const RIGHT_KEYS = ['right']
 export const ROTATE_LEFT_KEYS = ['ctl', 'z']
 export const ROTATE_RIGHT_KEYS = ['up', 'x']
 
+// When holding down a key, a player will move this many times per second....
 export const PLAYER_SIDEWAYS_RATE = 6
 export const PLAYER_DOWN_RATE = 20
+
+// Falling rate should be expressed in steps per second.
+export const INITIAL_FALL_RATE = 1
+
+// This number is added to the fall rate on each new level
+export const FALL_RATE_LEVEL_MODIFIER = 0.5
 
 // BOARD
 export const COLS = 10 //width
