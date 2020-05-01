69c4758c5d77ef2d6130b5ddb67a25bc697fe234
Rene Dellefont <renedellefont@gmail.com>
2020-05-01 11:43:12 -0400

    Add SRS wall kicks.

---
+++ src/wallKicks.js
@@ -0,0 +1,347 @@
+export const kicks = {
+  'JLSTZ': [
+    {
+      rotation: 0,
+      direction: -1,
+      tests:[
+        {
+          dx: 0, 
+          dy: 0
+        },
+        { 
+          dx:1,
+          dy:0
+        },
+        { 
+          dx:1,
+          dy:-1
+        },
+        { 
+          dx:0,
+          dy:2
+        },
+        { 
+          dx:1,
+          dy:2
+        },
+      ]
+    },
+    {
+      rotation: 0,
+      direction: 1,
+      tests:[
+        {
+          dx:0, dy:0
+        },
+        { 
+          dx:-1, dy:0
+        },
+        { 
+          dx:-1, dy:-1
+        },
+        { 
+          dx:0, dy:2
+        },
+        { 
+          dx:-1, dy:2
+        },
+      ]
+    },
+    {
+      rotation: 1,
+      direction: -1,
+      tests:[
+        {
+          dx:0, dy:0,
+        },
+        { 
+          dx:1, dy:0,
+        },
+        { 
+          dx:1, dy:1,
+        },
+        { 
+          dx:0, dy:-2,
+        },
+        { 
+          dx:1, dy:-2,
+        },
+      ]
+    },
+    {
+      rotation: 1,
+      direction: 1,
+      tests:[
+        {
+          dx:0, dy:0,
+        },
+        { 
+          dx:1, dy:0,
+        },
+        { 
+          dx:1, dy:1,
+        },
+        { 
+          dx:0, dy:-2,
+        },
+        { 
+          dx:1, dy:-2,
+        },
+      ]
+    },
+    {
+      rotation: 2,
+      direction: -1,
+      tests:[
+        {
+          dx:0, dy: 0,
+        },
+        { 
+          dx:-1, dy: 0,
+        },
+        { 
+          dx:-1, dy: -1,
+        },
+        { 
+          dx:0 , dy:2,
+        },
+        { 
+          dx:-1 , dy:2,
+        },
+      ]
+    },
+    {
+      rotation: 2,
+      direction: 1,
+      tests:[
+        {
+          dx: 0, dy:0,
+        },
+        { 
+          dx: 1, dy:0,
+        },
+        { 
+          dx: 1, dy:-1,
+        },
+        { 
+          dx: 0, dy:2,
+        },
+        { 
+          dx: 1, dy:2,
+        },
+      ]
+    },
+    {
+      rotation: 3,
+      direction: -1,
+      tests:[
+        {
+          dx:0, dy: 0,
+        },
+        { 
+          dx:-1, dy: 0,
+        },
+        { 
+          dx:-1 , dy:1,
+        },
+        { 
+          dx:0, dy: -2,
+        },
+        { 
+          dx:-1, dy: -2,
+        },
+      ]
+    },
+    {
+      rotation: 3,
+      direction: 1,
+      tests:[
+        {
+          dx: 0, dy:0,
+        },
+        { 
+          dx: -1, dy:0,
+        },
+        { 
+          dx: -1, dy:1,
+        },
+        { 
+          dx: 0, dy:-2,
+        },
+        { 
+          dx: -1, dy:-2,
+        },
+      ]
+    }
+  ],
+  'I': [
+    {
+      rotation: 0,
+      direction: -1,
+      tests:[
+        {
+          dx: 0, dy: 0,
+        },
+        { 
+          dx: -1, dy: 0,
+        },
+        { 
+          dx: 2, dy: 0,
+        },
+        { 
+          dx: -1, dy: -2,
+        },
+        { 
+          dx: 2, dy: 1,
+        },
+      ]
+    },
+    {
+      rotation: 0,
+      direction: 1,
+      tests:[
+        {
+          dx: 0, dy: 0,
+        },
+        { 
+          dx: -2, dy: 0,
+        },
+        { 
+          dx: 1, dy: 0,
+        },
+        { 
+          dx: -2, dy: 1,
+        },
+        { 
+          dx: 1, dy: -2,
+        },
+      ]
+    },
+    {
+      rotation: 1,
+      direction: -1,
+      tests:[
+        {
+          dx: 0, dy: 0,
+        },
+        { 
+          dx: 2, dy: 0,
+        },
+        { 
+          dx: -1, dy: 0,
+        },
+        { 
+          dx: 2, dy: -1,
+        },
+        { 
+          dx: -1, dy: 2,
+        },
+      ]
+    },
+    {
+      rotation: 1,
+      direction: 1,
+      tests:[
+        {
+          dx: 0, dy: 0,
+        },
+        { 
+          dx: -1, dy: 0,
+        },
+        { 
+          dx: 2, dy: 0,
+        },
+        { 
+          dx: -1, dy: -2,
+        },
+        { 
+          dx: 2, dy: 1,
+        },
+      ]
+    },
+    {
+      rotation: 2,
+      direction: -1,
+      tests:[
+        {
+          dx: 0, dy: 0,
+        },
+        { 
+          dx: 1, dy: 0,
+        },
+        { 
+          dx: -2, dy: 0,
+        },
+        { 
+          dx: 1, dy: 2,
+        },
+        { 
+          dx: -2, dy: -1,
+        },
+      ]
+    },
+    {
+      rotation: 2,
+      direction: 1,
+      tests:[
+        {
+          dx: 0, dy: 0,
+        },
+        { 
+          dx: 2, dy: 0,
+        },
+        { 
+          dx: -1, dy: 0,
+        },
+        { 
+          dx: 2, dy: -1,
+        },
+        { 
+          dx: -1, dy: 2,
+        },
+      ]
+    },
+    {
+      rotation: 3,
+      direction: 1,
+      tests:[
+        {
+          dx: 0, dy: 0,
+        },
+        { 
+          dx: -2, dy: 0,
+        },
+        { 
+          dx: 1, dy: 0,
+        },
+        { 
+          dx: -2, dy: 1,
+        },
+        { 
+          dx: 1, dy: -2,
+        },
+      ]
+    },
+    {
+      rotation: 3,
+      direction: 1,
+      tests:[
+        {
+          dx: 0, dy: 0,
+        },
+        { 
+          dx: 1, dy: 0,
+        },
+        { 
+          dx: -2, dy: 0,
+        },
+        { 
+          dx: 1, dy: 2,
+        },
+        { 
+          dx: -2, dy: -1,
+        },
+      ]
+    }
+  ]
+}
