1b1044bc3d831ebe6472a295e2ee18a5e342b09f
Rene Dellefont <renedellefont@gmail.com>
2020-05-06 15:47:48 -0400

    Add statistics

--- src/containers/Statistics.svelte
+++ src/containers/Statistics.svelte
@@ -1,7 +1,35 @@
 <script>
+  import { onMount, getContext } from 'svelte'
+
   import Display from '../components/Display.svelte'
+  import Piece from '../components/Piece.svelte'
+  import { TETRIS } from '../constants'
+
+  const { stats, tetrominos } = getContext(TETRIS)
 </script>
 
 <Display>
-  <span>Statistics</span>
+  <div>
+    <div class="title">Statistics</div>
+    {#each $stats as stat}
+      <div class="stat">
+        <Piece
+          width={45}
+          height={20}
+          piece={tetrominos[stat.id - 1]}
+          scale={0.5}
+          xOffset={(4 - tetrominos[stat.id - 1].matrix[0].length) / 2} />
+        <span>{stat.count.toString().padStart(3, '0')}</span>
+      </div>
+    {/each}
+  </div>
 </Display>
+
+<style>
+  .stat {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 5px;
+  }
+</style>
